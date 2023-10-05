af94d1bc07cb8c977181e186d30f59831ca8320d
Ez-FlawLess <amirali23mohammadi2@gmail.com>
2023-10-05 23:24:36 +0330

    finish day 6 part 2

--- src/bin/day_6_1/main.rs
+++ src/bin/day_6_1/main.rs
@@ -1,11 +1,14 @@
 use std::fs;
 
+// PART 1 = 4, PART 2 = 14
+const WINDOW_SIZE: usize = 14;
+
 fn main() {
     
     let data = fs::read_to_string("inputs\\day_6.txt").unwrap();
 
-    let result = data.as_bytes().windows(4).position(|window| {
-        let mut hold = Vec::with_capacity(4);
+    let result = data.as_bytes().windows(WINDOW_SIZE).position(|window| {
+        let mut hold = Vec::with_capacity(WINDOW_SIZE);
         for char in window {
             if hold.contains(char) {
                 return false;
@@ -15,5 +18,5 @@ fn main() {
         true
     });
 
-    println!("{}", result.unwrap() + 4);
+    println!("{}", result.unwrap() + WINDOW_SIZE);
 }
